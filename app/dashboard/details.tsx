f0358910e20603aaa9ba0054f0c2bfbc84edb06b
kmr-rohit <rk972006@student.nitw.ac.in>
2024-04-01 14:33:55 +0530

    Remove Unneccassary Details at dashboard

--- app/dashboard/details.tsx
+++ app/dashboard/details.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useOrganization, useSession, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { CopyIcon, Dot } from "../icons";
@@ -24,17 +24,6 @@ export function UserDetails() {
         boxShadow: `0px 20px 24px -4px rgba(16, 24, 40, 0.08)`,
       }}
     >
-      <div className="flex p-8">
-        <h3 className="text-xl leading-6 font-semibold text-gray-900 my-auto">
-          User
-        </h3>
-
-        <Toggle
-          checked={jsonOutput}
-          onChange={() => setJsonOutput(!jsonOutput)}
-          disabled={!isLoaded}
-        />
-      </div>
       {isLoaded && user ? (
         jsonOutput ? (
           <div className="overflow-scroll max-h-96 pb-6">
@@ -43,13 +32,6 @@ export function UserDetails() {
         ) : (
           <div className="pb-6 max-h-96">
             <dl>
-              <div className="px-8 py-2">
-                <dt className="text-sm font-semibold">User ID</dt>
-                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2">
-                  {user.id}
-                  <CopyButton text={user.id} />
-                </dd>
-              </div>
               {user.firstName && (
                 <div className="px-8 py-2">
                   <dt className="text-sm font-semibold mb-1">First Name</dt>
@@ -98,172 +80,6 @@ export function UserDetails() {
       ) : (
         <div className="text-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           Loading user data...
-        </div>
-      )}
-    </div>
-  );
-}
-
-export function SessionDetails() {
-  const { isLoaded, session } = useSession();
-  const [jsonOutput, setJsonOutput] = useState(false);
-
-  return (
-    <div
-      className="bg-white shadow overflow-hidden sm:rounded-lg"
-      style={{
-        boxShadow: `0px 20px 24px -4px rgba(16, 24, 40, 0.08)`,
-      }}
-    >
-      <div className="flex p-8">
-        <h3 className="text-xl leading-6 font-semibold text-gray-900 my-auto">
-          Session
-        </h3>
-        <Toggle
-          checked={jsonOutput}
-          onChange={() => setJsonOutput(!jsonOutput)}
-          disabled={!isLoaded}
-        />
-      </div>
-      {isLoaded && session ? (
-        jsonOutput ? (
-          <div className="overflow-scroll max-h-96 pb-6">
-            <JSONOutput
-              json={{
-                ...session,
-                user: undefined,
-              }}
-            />
-          </div>
-        ) : (
-          <div className="pb-6 max-h-96">
-            <dl>
-              <div className="px-8 py-2">
-                <dt className="text-sm font-semibold">Session ID</dt>
-                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2">
-                  {session.id}
-                  <CopyButton text={session.id} />
-                </dd>
-              </div>
-              <div className="px-8 py-2">
-                <dt className="text-sm font-semibold mb-1">Status</dt>
-                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                  {session.status === `active` && (
-                    <span className="text-xs bg-success-50 text-success-700 flex w-min gap-1 px-2 py-[1px] rounded-2xl font-medium">
-                      <div className="m-auto">
-                        <Dot />
-                      </div>
-                      Active
-                    </span>
-                  )}
-                </dd>
-              </div>
-              <div className="px-8 py-2">
-                <dt className="text-sm font-semibold mb-1">Last Active</dt>
-                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                  {session.lastActiveAt.toLocaleString()}
-                </dd>
-              </div>
-              <div className="px-8 py-2">
-                <dt className="text-sm font-semibold mb-1">Expiry</dt>
-                <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                  {session.expireAt.toLocaleString()}
-                </dd>
-              </div>
-            </dl>
-          </div>
-        )
-      ) : (
-        <div className="text-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          Loading user data...
-        </div>
-      )}
-    </div>
-  );
-}
-
-export function OrgDetails() {
-  const { isLoaded, organization } = useOrganization();
-  const [jsonOutput, setJsonOutput] = useState(false);
-
-  return (
-    <div
-      className="bg-white shadow overflow-hidden sm:rounded-lg"
-      style={{
-        boxShadow: `0px 20px 24px -4px rgba(16, 24, 40, 0.08)`,
-      }}
-    >
-      <div className="flex p-8">
-        <h3 className="text-xl leading-6 font-semibold text-gray-900 my-auto">
-          Organization
-        </h3>
-        <Toggle
-          checked={jsonOutput}
-          onChange={() => setJsonOutput(!jsonOutput)}
-          disabled={!(isLoaded && organization)}
-        />
-      </div>
-      {isLoaded ? (
-        organization ? (
-          jsonOutput ? (
-            <div className="overflow-scroll max-h-96 pb-6">
-              <JSONOutput json={organization} />
-            </div>
-          ) : (
-            <div className="pb-6 max-h-96">
-              <dl>
-                <div className="px-8 py-2">
-                  <dt className="text-sm font-semibold">Organization ID</dt>
-                  <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2 flex gap-2">
-                    {organization.id}
-                    <CopyButton text={organization.id} />
-                  </dd>
-                </div>
-                <div className="px-8 py-2">
-                  <dt className="text-sm font-semibold mb-1">Name</dt>
-                  <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                    {organization.name}
-                  </dd>
-                </div>
-                <div className="px-8 py-2">
-                  <dt className="text-sm font-semibold mb-1">Members</dt>
-                  <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                    {organization.membersCount}
-                  </dd>
-                </div>
-                <div className="px-8 py-2">
-                  <dt className="text-sm font-semibold mb-1">
-                    Pending invitations
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                    {organization.pendingInvitationsCount}
-                  </dd>
-                </div>
-                <div className="px-8 py-2">
-                  <dt className="text-sm font-semibold mb-1">Image</dt>
-                  <dd className="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
-                    <Image
-                      className="rounded"
-                      src={organization.imageUrl}
-                      alt={`Logo for ${organization.name}`}
-                      width={48}
-                      height={48}
-                    />
-                  </dd>
-                </div>
-              </dl>
-            </div>
-          )
-        ) : (
-          <div className="text-gray-700 px-8 pb-5 text-sm">
-            You are currently logged in to your personal workspace.
-            <br />
-            Create or switch to an organization to see its details.
-          </div>
-        )
-      ) : (
-        <div className="text-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          Loading organization data...
         </div>
       )}
     </div>
@@ -356,3 +172,5 @@ function JSONOutput(props: { json: any }) {
     </pre>
   );
 }
+
+
